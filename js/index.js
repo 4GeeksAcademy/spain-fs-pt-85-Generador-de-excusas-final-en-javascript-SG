7fc5132a710b237f4c3dd6ce8afdca8da7c27207
Sergi-03 <robrovskyyt@gmail.com>
2024-10-25 19:01:38 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+       for (let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i] + adj[a] + noun[b] + ".com");
+        
+        
+       }
+        
+    }
+    
+}
